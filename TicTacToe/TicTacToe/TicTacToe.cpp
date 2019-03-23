b3f1a8a9caf2f0028aa79303aa929fcb981188d2
- <emaildliaspama@mail.ru>
2019-03-23 23:21:14 +0300

    Broad Created, Can Change it

--- TicTacToe/TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe/TicTacToe.cpp
@@ -6,7 +6,64 @@
 
 using namespace std;
 
+class Desk 
+{
+	char Array[3][3];
+public:
+	Desk() 
+	{
+		for (int i = 0; i < 3;i++)
+			for (int j = 0; j < 3; j++)
+			{
+				Array[i][j] = ' ';
+			}
+	}
+	~Desk() {}
+	void ShowDesk()
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				cout << Array[i][j];
+				if (j < 3) cout << '|';
+			}
+			cout << endl;
+			for (int i = 0; i < 6; i++)
+				if (i % 2 == 0) cout << '-';
+				else cout << '+';
+			cout << endl;
+		}
+	}
+	void SetX() 
+	{ 
+		cout << "x="; 
+		int x = 1; 
+		int y = 1; 
+		cin >> x; 
+		cout << "y="; 
+		cin >> y; 
+		Array[y - 1][x - 1] = 'x'; 
+	}
+	void SetO() 
+	{ 
+		cout << "x="; 
+		int x = 1; 
+		int y = 1; 
+		cin >> x; 
+		cout << "y="; 
+		cin >> y; 
+		Array[y - 1][x - 1] = 'o';
+	}
+
+};
+
 int main()
 {
-
+	Desk Broad;
+	Broad.ShowDesk();
+	Broad.SetX();
+	Broad.ShowDesk();
+	Broad.SetO();
+	Broad.ShowDesk();
 }
